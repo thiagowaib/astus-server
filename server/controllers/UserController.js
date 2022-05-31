b31cd42cd6e85341ecc0adb031d933a415e08bd5
thiagowaib <thiagowaib@gmail.com>
2022-05-31 19:04:52 -0300

    Descriptive responses

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -13,11 +13,11 @@ const UserSignUp = (req, res) => {
                 password: await HashPwd(password)
             }
         })
+        res.status(201).send({message: "Usuário Cadastrado"})
     }
 
     main()
-        .then(res.sendStatus(201))
-        .catch((err)=>{res.status(500).send(err); throw err})
+        .catch((err)=>{res.status(400).send(err); throw err})
         .finally(async ()=>{await prisma.$disconnect()})
 }
 
@@ -31,24 +31,28 @@ const UserSignIn = (req, res) => {
               email: email,
             },
         })
-        if(await AuthPwd(user.password, password)) {
-            const payload = {
-                email: user.email,
-                name: user.name,
-                exp: SetExpDate(Date.now(), 1, "h")
+        if(user) {
+            if(await AuthPwd(user.password, password)) {
+                const payload = {
+                    email: user.email,
+                    name: user.name,
+                    exp: SetExpDate(Date.now(), 1, "h")
+                }
+                const accessToken= jwt.sign(
+                    payload,
+                    process.env.JWT_ACCESS_TOKEN_SECRET
+                )
+                res.status(202).send({accessToken, message: "Login bem-sucedido"})
+            } else {
+                res.status(401).send({message: "Senha Invalida"})
             }
-            const accessToken= jwt.sign(
-                payload,
-                process.env.JWT_ACCESS_TOKEN_SECRET
-            )
-            res.send({accessToken}).status(200)
+        } else {
+            res.status(404).send({message: "Email não Cadastrado"})
         }
-        else
-            res.sendStatus(400)
     }
 
     main()
-        .catch((err)=>{res.status(500).send(err); throw err})
+        .catch((err)=>{res.status(400).send(err); throw err})
         .finally(async ()=>{await prisma.$disconnect()})
 }
 
